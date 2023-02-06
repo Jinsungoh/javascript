924db08cb4413a494825464a35e5eb8ab214e3dc
오진성 <a@a.com>
2023-02-06 18:27:31 +0900

    2023-02-06

--- 3.object/3.1symbol.js
+++ 3.object/3.1symbol.js
@@ -11,3 +11,31 @@ console.log(id.toString())
 console.log(typeof id)
 //설명문 읽을떄씀 
 console.log(id1.description,id2.description,id.description)
+
+let user = {
+    userName:'neo'
+}
+
+let userId = Symbol('userId')
+user.userId = 1
+user[userId] = 2
+
+console.log(user)
+
+//과제:user를 iterating해서,key들을 출력하라.
+for(let key in user) console.log(key)
+
+console.log(Object.keys(user))
+
+id1 = Symbol.for('id')
+id2 = Symbol.for('id')
+console.log(id1 == id2)
+
+let key = Symbol.keyFor(id1)
+console.log(key,typeof key)
+
+id1 = Symbol.for('username')
+id2 = Symbol('userName')//description
+console.log(id1 == id2)
+
+console.log(Symbol.keyFor(id1),Symbol.keyFor(id2))
---
+++ 3.object/4.1constructor.js
@@ -0,0 +1,69 @@
+let makeUser = function(userName,age){
+    return{
+        userName: userName,
+        age: age
+    }
+}
+
+makeUser = function(userName,age){
+    return{
+        userName,
+        age
+    }
+}
+
+console.log(makeUser('doris',57))
+//생성자는 앞글자를 대문자로쓰고 명사로쓴다.new 를 붙여줘야 생성자가된다.
+function User(userName){
+    //this = {}
+    this.userName = userName
+    this.greet = () => console.log(`i am ${this.userName}.`)
+    //return this
+}
+
+let user1 = User('bob')
+user1 = new User('bob')
+
+console.log(user1)
+
+user1.greet()
+console.log(Object.keys(user1))
+
+const user2 = new User('sarah')
+user2.greet()
+
+let str = String(1)
+console.log(typeof str)
+
+str = new String(1)
+console.log(typeof str)
+
+let human = new function(){
+    this.humanName = 'meg'
+    this.age = 27
+}
+console.log(typeof human,human.humanName)
+
+function BigUser(){
+    this.userName = 'amy'
+    return {userName:'beth'}
+}
+
+function SmallUser(){
+    this.userName = 'lorie'
+}
+//과제:SmallUser().userName 을 고쳐서,error가 발생하지 않도록 하라.
+console.log(BigUser().userName,SmallUser()?.userName)
+
+const user = new Object()
+console.log(user)
+user.age = 12
+
+//view object
+const person = Object.create(user)
+console.log(person)
+console.log(person.age)
+console.log(person == user)
+
+person.personName = 'mandarin'
+console.log(user)
