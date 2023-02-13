c55d234d177069df92762fbbccf36a7e63f8bef7
오진성 <a@a.com>
2023-02-13 10:16:40 +0900

    2023-02-13

--- 8.promise/home/2.jajangmyeon.js
+++ 8.promise/home/2.jajangmyeon.js
@@ -1,6 +1,8 @@
 /*
 4.class/home/2.jajangmyeon.js 를 promise 로 refactoring 하라.
 */
+
+
 class Food {
     constructor(taste, foodName) {
         this.taste = taste
@@ -14,20 +16,23 @@ class Food {
 
 class Chef {
     constructor(taste){
-        this.cook = foodName => new Food(taste, foodName)
-        
+        this.taste = taste
     }
+
+   cook(foodName) {
+    return new Promise((resolve, reject) => 
+            setTimeout(() => resolve(new Food(this.taste,foodName)), 1000))
+   }
 }
 
 class Waiter {
     async order(foodName, chef){
-        return await Promise.setTimeout(()=>resolve(foodName,chef),1000) 
+        return await chef.cook(foodName)
     }
 }
 
 let chef1 = new Chef('달콤한')
 let chef2 = new Chef('매운')
 let waiter = new Waiter()
-    
-//console.log(waiter.order('짜장면', chef1))
-//console.log(`${waiter.order('짬뽕', chef2)}`)
+
+waiter.order('짜장면', chef1).then(console.log)
